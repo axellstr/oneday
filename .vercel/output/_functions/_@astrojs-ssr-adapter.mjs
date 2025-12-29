61964b8f772ed4925c34213936dd078e46e3cea7
Your Name <your.email@example.com>
2025-12-29 18:43:53 +0200

    chore: add .vercel to gitignore and remove from tracking

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, a as start } from './chunks/_@astrojs-ssr-adapter_LFlWw4d8.mjs';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_CZVUxw1T.mjs';
-export { renderers } from '../renderers.mjs';
